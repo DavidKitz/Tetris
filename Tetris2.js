3a750683fd10c91fe7522b2bd6f05eef15521dfc
David Kitz <david.kitzwork@gmail.com>
2020-09-08 20:17:52 +0200

    trying to rotate the 3x3 array correctly

--- Tetris2.js
+++ Tetris2.js
@@ -18,6 +18,10 @@ const player= {
         y:-50
     }
 }
+function callDraw() {
+    player.position.y+=box;
+    draw(player.matrix,player.position);
+}
 
 function collision () {
    
@@ -34,18 +38,28 @@ function collision () {
     return false;
 
 } 
-function collisionSide(newX,newY) {
-    if (newX<0 || newX > 225) {
+function collisionSide(newX) {
+    
+    if (newX<0) {
+        player.position.x+=box;
         return true;
-
     }
-}
+    else if(newX > 225) {
+        console.log("hello");
+        player.position.x-=box;
+        return true;
+    }
+    return false;
+}  
+
+       
+
 function draw(matrix,offset) {
     if (player.position.y===450 || collision() ) {
         player.position.y-=box;
         clearInterval(ticker);
         merge(gameMatrix,player);
-        player.position.y=-50;
+        player.position.y=-25;
         player.position.x=0;
         ticker=setInterval(callDraw,1000);
     }
@@ -54,19 +68,29 @@ function draw(matrix,offset) {
     drawGameMatrix (gameMatrix);
     piece.forEach((row,y) => {
         row.forEach((column,x) => {
-
-            if (column!==0) {
-                
+            if (column!==0) { 
                 ctx.fillStyle="Green";
                 ctx.fillRect(box*x+offset.x,y*box+offset.y,box,box);
                 ctx.strokeStyle= "black";
                 ctx.strokeRect(box*x+offset.x,y*box+offset.y,box,box);
-               
             }
         })
     });
 }
+function drawGameMatrix (gameMatrix) {
+    gameMatrix.forEach((row,y) => {
+        row.forEach((column,x) => {
+            if (column!==0) {
+                ctx.fillStyle="Green";
+                ctx.fillRect(box*x,y*box,box,box);
+                ctx.strokeStyle= "black";
+                ctx.strokeRect(box*x,y*box,box,box);
 
+            }
+        })
+    })
+
+}
 
 function merge(gameMatrix,player) {
     
@@ -78,27 +102,9 @@ function merge(gameMatrix,player) {
             }
         });
     });
-    
-    
 }
 
-function drawGameMatrix (gameMatrix) {
-    gameMatrix.forEach((row,y) => {
-        row.forEach((column,x) => {
-            if (column!==0) {
-                ctx.fillStyle="Green";
-                ctx.fillRect(box*x,y*box,box,box);
-                ctx.strokeStyle= "black";
-                ctx.strokeRect(box*x,y*box,box,box);
 
-            }
-
-            
-
-        })
-    })
-
-}
 function move (event) {
     if (event.keyCode===40) {
         if (player.position.y===450|| collision()) {
@@ -115,8 +121,8 @@ function move (event) {
             }
     }
     if (event.keyCode===39) {
-       if (!collisionSide(player.position.x+=box,player.position.y)) {
-        player.position.x+=box
+       if (!collisionSide(player.position.x+=box)) {
+        
         draw(player.matrix,player.position);
        }
     }
@@ -124,26 +130,38 @@ function move (event) {
            
     }
     if (event.keyCode===37) {
-        if (!collisionSide(player.position.x-=box,player.position.y)) {
-        player.position.x-=box;
+        if (!collisionSide(player.position.x-=box)) {
+        
         draw(player.matrix,player.position);
         }
-        else {
-            return;
-        }
+       
     }
     
     }
-    function callDraw() {
-        player.position.y+=box;
-    //    if (collision())  {    
-    //     player.position.y-=box;
-    //     merge(gameMatrix,player);
-    //     player.position.y=-50;
-    //     player.position.x=0;
-    //  }
     
-        draw(player.matrix,player.position);
+function rotate() {
+let newMatrix=Array(3).fill().map(() => Array(3).fill(0));
+
+
+
+const result=player.matrix.map((row,x) => { 
+    row.map((value,y)  => {
+   newMatrix[(player.matrix.length-1)-y][x]=player.matrix[(player.matrix.length-1)-y][x]
+        
+         
+    })  
+});
+
+
+
+
+console.log(newMatrix);
+
     
-    }
+}
+
+
+
+
+
 let ticker=setInterval(callDraw,1000);
