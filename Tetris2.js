cc92239dca7361c429a178f312c232e1e2594157
David Kitz <david.kitzwork@gmail.com>
2020-09-16 15:20:22 +0200

    fixed draw tetris function
    collision side function still needs works - see comments

--- Tetris2.js
+++ Tetris2.js
@@ -1,6 +1,6 @@
 //collision works because as soon as piece goes of gameMatrix field it produces undefined which turns !==0 to true
 //So as soon as piece touches bottom border collision returns true
-//piece 2 has earlier impact duo to one more array length size so needs to stop not to earlie with y coordinate condition at draw()
+//When pressing left or right on the arrow keys they merge inside the other pieces, need to detect the collision if these buttons are pressed
 const cnv= document.getElementById("Gameboard");
 const ctx= cnv.getContext("2d");
 const box= 25;
@@ -99,65 +99,84 @@ function checkForTetris() {
                 count++
                 if (count===12) {
                     nbrs.push(y);
-                    //drawAfterTetris(y);
                 }
             }
             
-    })
-        
+        })   
     })
     if(nbrs.length!==0) {
-   nbrs.sort((a, b)=>{return b-a});
-    for(let i=0;i<nbrs.length;i++) {
-        drawAfterTetris(nbrs[i]);
-   }
-}
-}
-function drawAfterTetris(nbr) {
-console.log(nbr);
-    gameMatrix[nbr].forEach((value,x)=> {
-        gameMatrix[nbr][x]=0;        
-    })
-    for (let i=gameMatrix.length-1;i>=0;i--) {
-  
-        gameMatrix[nbr-i].forEach((value,x)=> {
-            if(value!==0) {
-                gameMatrix[nbr-i][x]=gameMatrix[nbr][x];
-                gameMatrix[nbr][x]=1;
-            }
-        }
-        )
-    
+         nbrs.sort((a, b)=>{return b-a});  
+          drawAfterTetris(...nbrs);  
     }
-    
+}
+function drawAfterTetris(...arrayTetris) {
+
+let tetrisNbrs=arrayTetris;
+console.log(tetrisNbrs);
+    for (let i=0;i<tetrisNbrs.length;i++) {
+      gameMatrix[tetrisNbrs[i]].forEach((value,x)=> {
+          gameMatrix[tetrisNbrs[i]][x]=0;        
+        })
+    }
+    for (let i=0;i<=tetrisNbrs[tetrisNbrs.length-1];i++) {
+        if(gameMatrix[(tetrisNbrs[tetrisNbrs.length-1])-1-i]!==undefined) {
+            gameMatrix[(tetrisNbrs[tetrisNbrs.length-1])-1-i].forEach((value,x)=> {
+                if(value!==0) {
+                    gameMatrix[(tetrisNbrs[tetrisNbrs.length-1])-1-i][x]= gameMatrix[(tetrisNbrs[0])-i][x];
+                    gameMatrix[(tetrisNbrs[0])-i][x]=1;
+                }
+                })
+        }
+    }   
+   
 }
 
+function reposition() {
 
+    // gameMatrix.forEach((row,y)=> {
+    //     row.forEach((value,x)=> {
+    //         if(value!==0 && ((player.position.x/box+x)===))
+
+    //     })
+    // })
+    for (let i=0;i<player.matrix.length;i++) {
+        for (let x=0;x<player.matrix.length;x++) {
+            if(player.matrix[i][x]!==0 &&
+            (gameMatrix[i + (player.position.y/box)] && 
+            gameMatrix[i + (player.position.y/box)][x + (player.position.x/box)]) !==0 ) {
+                player.position.x+=box;
+            }
+}
+    }
+}
        
 
 function draw(matrix,offset) {
-    if (player.position.y===450 && player.matrix[0].length!== 4 || collision() ) {
+    if (collision()) {
+        
+        console.log("i turn true now");
         player.position.y-=box;
         clearInterval(ticker);
         merge(gameMatrix,player);
+        checkForTetris();
         player.position.y=-25;
         player.position.x=0;
         ticker=setInterval(callDraw,1000);
         player.matrix= generatePiece();
     }
-    ctx.fillStyle="White";
-    ctx.fillRect(0,0,300,500);
-    drawGameMatrix (gameMatrix);
-    player.matrix.forEach((row,y) => {
-        row.forEach((column,x) => {
-            if (column!==0) { 
-                ctx.fillStyle="Green";
-                ctx.fillRect(box*x+offset.x,y*box+offset.y,box,box);
-                ctx.strokeStyle= "black";
-                ctx.strokeRect(box*x+offset.x,y*box+offset.y,box,box);
-            }
-        })
-    });
+ctx.fillStyle="White";
+ctx.fillRect(0,0,300,500);
+drawGameMatrix (gameMatrix);
+player.matrix.forEach((row,y) => {
+    row.forEach((column,x) => {
+        if (column!==0) { 
+            ctx.fillStyle="Green";
+            ctx.fillRect(box*x+offset.x,y*box+offset.y,box,box);
+            ctx.strokeStyle= "black";
+            ctx.strokeRect(box*x+offset.x,y*box+offset.y,box,box);
+        }
+    })
+});
 }
 function drawGameMatrix (gameMatrix) {
     gameMatrix.forEach((row,y) => {
@@ -247,34 +266,28 @@ function move (event) {
         }
        
     }
-    
-    }
+}
     
 function rotate() {
     if (player.matrix[0].length === 4) {
-        console.log("hello");
         let newMatrix=[[],[],[],[]];
         player.matrix.map((row,x) => { 
             row.map((value,y)  => {
                 
            newMatrix[x].push(player.matrix[(player.matrix.length-1)-y][x])
-                
-                 
             })  
         });
         player.matrix=newMatrix;
     }
     else {
-let newMatrix2=[[],[],[]];
- player.matrix.map((row,x) => { 
-    row.map((value,y)  => {
-        
-   newMatrix2[x].push(player.matrix[(player.matrix.length-1)-y][x])
-        
-         
-    })  
-});
-player.matrix=newMatrix2;
+    let newMatrix2=[[],[],[]];
+    player.matrix.map((row,x) => { 
+        row.map((value,y)  => {
+       
+            newMatrix2[x].push(player.matrix[(player.matrix.length-1)-y][x])   
+         })  
+    });
+    player.matrix=newMatrix2;
     }
 
 checkRotatePosition();
